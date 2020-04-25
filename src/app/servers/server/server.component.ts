f409c9d3d1abfd2d60141240aab031d77c2c666e
Definkle <52527078+Definkle@users.noreply.github.com>
2020-04-25 22:23:39 +0800

    initial commit

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit, OnDestroy {
+  server: {id: number, name: string, status: string};
+  paramsSubscription: Subscription;
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute,
+              private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.paramsSubscription = this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.paramsSubscription.unsubscribe();
+  }
+
+  edit() {
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
+  }
+}
